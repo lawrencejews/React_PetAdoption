5e1be6c053387bec80311b6a841a4c9f22f572af
lawrencejews <lawrencejews1@gmail.com>
2022-05-29 17:47:32 +0900

    added JS tools and dependencies

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Pet Adoption with React-version-17
-## 
+## Different step for this project
+- Intro to Vanila javascript
+- Installation of project tools and dependencies:`format-check for github actions, format-write for code development and  lint for CI $ CD pipelines`
+- Core React Concepts
+- 
--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h3", {}, props.breed)
-  ]);
-}
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
-    
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
-      name: 'Pepper',
-      animal: 'bird',
-      breed: 'Sun'
+      name: "Pepper",
+      animal: "bird",
+      breed: "Sun",
     }),
     React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'Dog',
-      breed: 'Havanese'
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
     }),
     React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'Cat',
-      breed: 'Silver'
+      name: "Luna",
+      animal: "Cat",
+      breed: "Silver",
     }),
   ]);
-}
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+};
+render(React.createElement(App), document.getElementById("root"));
