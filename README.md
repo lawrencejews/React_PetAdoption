4809dbcb7ddb91f6d300190a6b0fe8c2592a1b23
lawrencejews <lawrencejews1@gmail.com>
2022-05-30 23:29:53 +0900

    core concepts intro

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Pet Adoption with React-version-17
-## Different step for this project
+## Introduction to React
 - Intro to Vanila javascript
 - Installation of project tools and dependencies:`format-check for github actions, format-write for code development and  lint for CI $ CD pipelines`
-- Core React Concepts
+## Core React Concepts
+- Eslint Addition feature libraries for React, jsx for accessibility and import for logic across files.
+- Rules for redundant react types and settings for the version of react.
+- Hooks =  add statefulness in the application where data is rendered follow an order.
+- Hooks should never be put in if statements or loops i.e protection pkg `eslint-plugin-react-hooks@4.3.0`
 - 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "bird",
-      breed: "Sun",
-    }),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Cat",
-      breed: "Silver",
-    }),
-  ]);
-};
-render(React.createElement(App), document.getElementById("root"));
+  return (
+    <div>
+      <h1>Adopt Me! </h1>
+      <SearchParams />
+    </div>
+  );
+}
+render( <App/>, document.getElementById("root"));
